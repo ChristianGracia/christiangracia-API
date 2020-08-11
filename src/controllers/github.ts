1dbcbd4dc9353585f02de4b4f04db571430fb277
Christian Gracia <christiangracia.student@careerdevs.com>
2020-08-11 00:24:36 -0400

    route is now returning all my repos as json stripped to just the info I want

--- src/controllers/github.ts
+++ src/controllers/github.ts
@@ -6,13 +6,20 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 router.get('/all-repos', async (req: Request, res: Response) => {
-  console.log('hi');
   const clientId: string = process.env.gitId;
   const clientSecret: string = process.env.gitSecret;
-  const URL: string = `https://api.github.com/users/ChristianGracia/repos?per_page=10&sort=createdasc&client_id=${clientId}&client_secret=${clientSecret}`;
+  const URL: string = `https://api.github.com/users/ChristianGracia/repos?per_page=20&sort=createdasc&client_id=${clientId}&client_secret=${clientSecret}`;
 
   const json = await returnExternalGet(URL);
-  res.send(json);
+  const reducedJson = [];
+  json.forEach((repo: any) => {
+    let newObj: any = {};
+    newObj.url = repo.name;
+    newObj.description = repo.description;
+    newObj.name = repo.html_url;
+    reducedJson.push(newObj);
+  });
+  res.send(reducedJson);
 });
 
 module.exports = router;
