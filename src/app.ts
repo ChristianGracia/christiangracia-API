d35fcfcc77e6f2432094c45a79895977bf87ab81
Christian Gracia <christiangracia.student@careerdevs.com>
2020-08-12 00:05:40 -0400

    set up mailgun and created a route to send me emails from my website

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 // import * as express from 'express';
-import express, { Request, Response } from 'express';
-import * as bodyParser from 'body-parser';
-import * as path from 'path';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import * as bodyParser from "body-parser";
+import * as path from "path";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,43 +14,46 @@ app.use(router);
 
 const options: cors.CorsOptions = {
   allowedHeaders: [
-    'Origin',
-    'X-Requested-With',
-    'Content-Type',
-    'Accept',
-    'X-Access-Token',
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "X-Access-Token",
   ],
   credentials: true,
-  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: '*',
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  origin: "*",
   preflightContinue: false,
 };
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json",
   );
   next();
 });
 
 router.use(cors(options));
 
-router.options('*', cors(options));
+router.options("*", cors(options));
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Welcome to my API back-end</h1>');
+router.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Welcome to my API back-end</h1>");
 });
 
-const githubController = require('./controllers/github');
-app.use('/github', githubController);
+const githubController = require("./controllers/github");
+app.use("/github", githubController);
+
+const emailController = require("./controllers/email");
+app.use("/email", emailController);
 
 export default app;
---
+++ src/controllers/email.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+const router = express.Router();
+
+import * as dotenv from "dotenv";
+
+import { NodeMailgun } from "ts-mailgun";
+
+const mailer = new NodeMailgun();
+mailer.apiKey = process.env.MAILGUN_API_KEY;
+mailer.domain = process.env.MAILGUN_DOMAIN;
+mailer.fromEmail = "noreply@christiangracia.com";
+mailer.fromTitle = "New email from christiangracia.com";
+mailer.init();
+
+dotenv.config();
+
+router.get("/send-email", (req: Request, res: Response) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  const emailTitle = `${name}: ${email} | christiangracia.com`;
+
+  mailer
+    .send("christianmgracia@gmail.com", emailTitle, `<h1>${message}</h1>`)
+    .then((result) => res.send({}))
+    .catch((error) => res.send(null));
+  // });
+});
+
+module.exports = router;
