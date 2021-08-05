8e5f96932a5f55eb63ee451c6fa80f517d823d8c
Christian Gracia <christiangracia.student@careerdevs.com>
2021-08-04 22:13:48 -0500

    made all repo's commits mapping better

--- src/controllers/github/github.ts
+++ src/controllers/github/github.ts
@@ -41,15 +41,13 @@ router.get('/repo-all-commits', async (req: Request, res: Response) => {
   const URL = `https://api.github.com/repos/christiangracia/${repoName}/commits?per_page=${amount}&client_id=${clientId}&client_secret=${clientSecret}`;
 
   const json = await returnExternalGet(URL);
-  const reducedJson = [];
-  json.forEach((commit: any) => {
-    const newObj: any = {};
 
-    newObj.time = commit.commit.author.date;
-    newObj.message = commit.commit.message;
-    newObj.url = commit.html_url;
-
-    reducedJson.push(newObj);
+  const reducedJson = json.map((commit: any) => {
+    return {
+      time: commit.commit.author.date,
+      message: commit.commit.message,
+      url: commit.html_url,
+    };
   });
   res.send(reducedJson);
 });
