2bbfaaf0fb750cb3ea854749a4eb913a62e8afff
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-13 18:45:29 -0500

    login almost done with puppeteer

--- src/routes/stocktwits.ts
+++ src/routes/stocktwits.ts
@@ -20,10 +20,46 @@ router.post('/', async (req: Request, res: Response) => {
     headless: false,
     ignoreHTTPSErrors: true,
     args: ['--no-sandbox'],
+    userAgent:
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36',
+    defaultViewport: {
+      width: 1600,
+      height: 1000,
+    },
   };
   const browser = await puppeteer.launch(browserOptions);
   const page = await browser.newPage();
-  page.goto('https://www.stocktwits.com');
+
+  await page.goto('https://www.stocktwits.com', {
+    // waitUntil: 'networkidle0',
+  });
+
+  const loginButton = await page.$x(
+    '//*[@id="app"]/div/div/div[2]/div/div/div/div/div[1]/div[1]/div[3]/div[2]/span',
+  );
+
+  await loginButton[0].click();
+
+  // Enter login credentials
+  await page.$eval(
+    'input[name=login]',
+    (el: any, value) => (el.value = value),
+    'dhdhdhd',
+  );
+
+  await page.$eval(
+    'input[name=password]',
+    (el: any, value) => (el.value = value),
+    'dhdhedhedh',
+  );
+
+  const submitButton = await page.$x(
+    '//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[2]/div[1]/button',
+  );
+
+  await submitButton[0].click();
+
+  // await browser.close();
 });
 
 router.get('/', (req: Request, res: Response) => {
