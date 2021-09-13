e539873fc76be4ea6af0ed325a87a3d8a1d56d7b
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-12 19:22:07 -0500

    puppeteer working

--- src/routes/stocktwits.ts
+++ src/routes/stocktwits.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from 'express';
+import puppeteer from 'puppeteer';
 
 const router = express.Router();
 
-router.post('/', (req: Request, res: Response) => {});
+router.post('/', async (req: Request, res: Response) => {
+  const browserOptions = {
+    headless: false,
+    ignoreHTTPSErrors: true,
+    args: ['--no-sandbox'],
+  };
+  const browser = await puppeteer.launch(browserOptions);
+  const page = await browser.newPage();
+  page.goto('https://www.stocktwits.com');
+});
 
 router.get('/', (req: Request, res: Response) => {
   res.send('hi');
