449b0b930349ee822be75031a6e2f86aaf058123
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-13 19:42:43 -0500

    bot now posting to stocktwits

--- src/routes/stocktwits.ts
+++ src/routes/stocktwits.ts
@@ -30,41 +30,41 @@ router.post('/', async (req: Request, res: Response) => {
   const browser = await puppeteer.launch(browserOptions);
   const page = await browser.newPage();
 
-  await page.goto('https://www.stocktwits.com', {
-    // waitUntil: 'networkidle0',
-  });
+  await page.goto('https://www.stocktwits.com');
 
   const loginButton = await page.$x(
     '//*[@id="app"]/div/div/div[2]/div/div/div/div/div[1]/div[1]/div[3]/div[2]/span',
   );
-
   await loginButton[0].click();
 
-  // Enter login credentials
   await page.type('input[name=login]', process.env.ST_USERNAME);
   await page.type('input[name=password]', process.env.ST_PASSWORD);
 
   const submitButton = await page.$x(
     '//*[@id="app"]/div/div/div[4]/div[2]/div/form/div[2]/div[1]/button',
   );
-
   await submitButton[0].click();
+
   await page.waitForTimeout(2000);
+
   const formattedMessage = `$${ticker} ${message}`;
 
   const sentimentXpath = `//*[@id="app"]/div/div/div[3]/div[2]/div/div[1]/div/div/div[1]/div/div[2]/div[2]/div/div[1]/div/div[${
     positiveSentiment ? '1' : '2'
   }]`;
-
   const sentiment = await page.$x(sentimentXpath);
   await sentiment[0].click();
 
-  //   message_input = self.driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div')[0]
-  //   message_input.click()
-  //   message_input.send_keys(formatted_message)
+  const messageInput = await page.$x(
+    '//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[2]/div[1]/div/div/div[2]/div',
+  );
+  await messageInput[0].click();
+  await page.keyboard.type(formattedMessage);
 
-  //   submit_post_button = self.driver.find_elements_by_xpath('//*[@id="app"]/div/div/div[3]/div/div/div[1]/div/div/div[1]/div/div[3]/div[1]/button')[0]
-  //   submit_post_button.click()
+  const postButton = await page.$x(
+    '//*[@id="app"]/div/div/div[3]/div[2]/div/div[1]/div/div/div[1]/div/div[3]/div[1]/button',
+  );
+  await postButton[0].click();
 
   // await browser.close();
 });
