2551b978db11c9373fda1903ea729984d79704ef
Christian Gracia <christiangracia.student@careerdevs.com>
2021-12-13 23:14:08 -0600

    fix

--- src/routes/spotify.ts
+++ src/routes/spotify.ts
@@ -3,18 +3,18 @@ import makeRequest from '../services/fetch-service';
 
 const router = express.Router();
 
-router.post('/current-song', async (req: Request, res: Response) => {
-  const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-  // const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
-  const json = await makeRequest('https://api.spotify.com/v1/me', {
-    method: 'get',
-    headers: {
-      Authorization: 'Bearer ' + client_secret,
-    },
-    json: true,
-  });
-  res.send(json);
-});
+// router.post('/current-song', async (req: Request, res: Response) => {
+//   const client_id = process.env.SPOTIFY_CLIENT_ID;
+//   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+//   // const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN
+//   const json = await makeRequest('https://api.spotify.com/v1/me', {
+//     method: 'get',
+//     headers: {
+//       Authorization: 'Bearer ' + client_secret,
+//     },
+//     json: true,
+//   });
+//   res.send(json);
+// });
 
 module.exports = router;
