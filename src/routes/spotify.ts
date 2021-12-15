d69b3dc206a9242d5d4147396058f84ad6fa83ad
Christian Gracia <christiangracia.student@careerdevs.com>
2021-12-14 21:09:30 -0600

    route set up

--- src/routes/spotify.ts
+++ src/routes/spotify.ts
@@ -1,8 +1,8 @@
-const express = require('express');
-const request = require('request');
-const cors = require('cors');
-const querystring = require('querystring');
-const cookieParser = require('cookie-parser');
+import express, { Request, Response } from 'express';
+import request from 'request';
+import querystring from 'querystring';
+import { spotifyService } from '../services/spotify-service';
+import * as path from 'path';
 
 const router = express.Router();
 
@@ -28,7 +28,7 @@ const generateRandomString = function (length) {
 
 const stateKey = 'spotify_auth_state';
 
-router.get('/login', function (req, res) {
+router.get('/login', (req: Request, res: Response) => {
   const state = generateRandomString(16);
   res.cookie(stateKey, state);
   const scope = 'user-read-private user-read-email user-read-currently-playing';
@@ -44,7 +44,7 @@ router.get('/login', function (req, res) {
   );
 });
 
-router.get('/callback', function (req, res) {
+router.get('/callback', (req: Request, res: Response) => {
   const code = req.query.code || null;
   const state = req.query.state || null;
   const storedState = req.cookies ? req.cookies[stateKey] : null;
@@ -75,8 +75,8 @@ router.get('/callback', function (req, res) {
 
     request.post(authOptions, function (error, response, body) {
       if (!error && response.statusCode === 200) {
-        const access_token = body.access_token,
-          refresh_token = body.refresh_token;
+        const access_token = body.access_token;
+        const refresh_token = body.refresh_token;
 
         const options = {
           url: 'https://api.spotify.com/v1/me/player/currently-playing',
@@ -86,15 +86,16 @@ router.get('/callback', function (req, res) {
 
         request.get(options, function (error, response, body) {
           console.log(body);
+          res.status(200).send(spotifyService.formatCurrentSong(body));
         });
 
-        res.redirect(
-          '/#' +
-            querystring.stringify({
-              access_token: access_token,
-              refresh_token: refresh_token,
-            }),
-        );
+        // res.redirect(
+        //   '/#' +
+        //     querystring.stringify({
+        //       access_token: access_token,
+        //       refresh_token: refresh_token,
+        //     }),
+        // );
       } else {
         res.redirect(
           '/#' +
@@ -107,8 +108,7 @@ router.get('/callback', function (req, res) {
   }
 });
 
-router.get('/refresh_token', function (req, res) {
-  // requesting access token from refresh token
+router.get('/refresh_token', (req: Request, res: Response) => {
   const refresh_token = req.query.refresh_token;
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
@@ -132,6 +132,10 @@ router.get('/refresh_token', function (req, res) {
       });
     }
   });
+});
+
+router.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../views/spotify.html'));
 });
 
 module.exports = router;
---
+++ src/services/spotify-service.ts
@@ -0,0 +1,17 @@
+export const spotifyService = {
+  /**
+   * format spotify data for current song
+   * @param { * } song - current song data
+   */
+  formatCurrentSong: (song: any): any => {
+    return [
+      {
+        progress: song.progress_ms,
+        duration: song.item.duration_ms,
+        artist: song.item.album.artists[0].name,
+        song: song.item.name,
+        images: song.item.album.images,
+      },
+    ];
+  },
+};
