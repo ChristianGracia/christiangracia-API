e9000afd6dbec05818f348c96fc732420db755d8
Christian Gracia <christiangracia.student@careerdevs.com>
2021-12-15 18:43:18 -0600

    add for deploy

--- src/routes/spotify.ts
+++ src/routes/spotify.ts
@@ -40,7 +40,7 @@ router.get('/login', async function (req, res) {
   const browserOptions = {
     headless: true,
     ignoreHTTPSErrors: true,
-    args: ['--no-sandbox'],
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
     dumpio: true,
     userAgent:
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36',
