d65eb1f819fe4e394f48fec70ff4bacc0f6fc1ad
Christian Gracia <christiangracia.student@careerdevs.com>
2021-12-15 22:32:23 -0600

    added concurrency

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 import app from './app';
 
-const server = app.listen(app.get('port'), () => {
-  console.log(
-    '  App is running at http://localhost:%d in %s mode',
-    app.get('port'),
-    app.get('env'),
-  );
+const throng = require('throng');
+
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+
+function start() {
+  app.listen(app.get('port'), () => {
+    console.log(
+      '  App is running at http://localhost:%d in %s mode',
+      app.get('port'),
+      app.get('env'),
+    );
+  });
+}
+
+throng({
+  workers: WORKERS,
+  lifetime: Infinity,
+  start,
 });
 
-export default server;
+export default start;
