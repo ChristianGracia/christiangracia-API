1a720a94a8c94151892fec39563db58881f598e8
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2021-12-21 22:04:45 -0600

    quick refactor

--- src/routes/github.ts
+++ src/routes/github.ts
@@ -1,27 +1,33 @@
 import express, { Request, Response } from 'express';
 const router = express.Router();
-import makeRequest from '../services/fetch-service';
+const axios = require('axios');
 import { githubService } from '../services/github-service';
-import * as path from 'path';
 
 router.get('/all-repos', async (req: Request, res: Response) => {
   const { gitId, gitSecret } = process.env;
-
-  const URL = `https://api.github.com/users/ChristianGracia/repos?per_page=20&sort=createdasc&client_id=${gitId}&client_secret=${gitSecret}`;
-
-  const json = await makeRequest(URL);
-
-  res.send(json.length ? githubService.formatRepos(json) : json);
+  const response = await axios({
+      url: `https://api.github.com/users/ChristianGracia/repos?per_page=20&sort=createdasc&client_id=${gitId}&client_secret=${gitSecret}`
+  });
+  if (response.status === 200) {
+    const { data } = response;
+    res.status(200).send(data.length ? githubService.formatRepos(data) : []);
+  } else {
+    res.status(response.status).json(response);
+  }
 });
 
 router.get('/repo-all-commits', async (req: Request, res: Response) => {
   const { clientId, clientSecret } = process.env;
   const { repoName, amount } = req.query;
-
-  const URL = `https://api.github.com/repos/christiangracia/${repoName}/commits?per_page=${amount}&client_id=${clientId}&client_secret=${clientSecret}`;
-
-  const json = await makeRequest(URL);
-  res.send(githubService.formatCommits(json));
+  const response = await axios({
+    url: `https://api.github.com/repos/christiangracia/${repoName}/commits?per_page=${amount}&client_id=${clientId}&client_secret=${clientSecret}`
+  });
+  if (response.status === 200) {
+    const { data } = response;
+    res.status(200).json(githubService.formatCommits(data));
+  } else {
+    res.status(response.status).json(response);
+  }
 });
 
 module.exports = router;
