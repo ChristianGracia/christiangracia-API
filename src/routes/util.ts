7698ec48a0c01f392fb1efb5ef5eef1fdb35b60a
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-01-31 00:14:41 -0600

    route clean

--- src/routes/util.ts
+++ src/routes/util.ts
@@ -11,15 +11,7 @@ router.get('/show-file', async (req: Request, res: Response) => {
     //     res.status(400).send('File to be viewed is required')
     // }
     res.setHeader('content-type', 'text/html');
-    console.log('1')
     res.status(200).send(await utilService.parseCodeFileToHtml(req.query.file.toString(), req.query.cssFile.toString()));
-    // const response = await utilService.parseCodeFileToHtml(file.toString(), cssFile.toString());
-    // if (response) {
-    //     res.setHeader('content-type', 'text/html');
-    //     res.status(200).send(response);
-    //   } else {
-    //     res.status(500).json({});
-    //   }
 });
 
 
